fde029db365ef526a703cbb911a7ccab3f474089
Simon Jentsch <simon@bojagi.io>
2025-07-09 12:13:23 +0200

    Add npm publish workflow

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,17 @@ export default defineConfig({
   build: {
     lib: {
       entry: {
-        'jotai-related': resolve(__dirname, 'src/index.ts'),
+        'jotai-portal': resolve(__dirname, 'src/index.ts'),
       },
-      name: 'jotai-related',
+      name: 'jotai-portal',
     },
     rollupOptions: {
       external: ['react', 'jotai/vanilla', 'jotai/vanilla/utils'],
       output: {
         globals: {
           react: 'react',
+          'jotai/vanilla': 'jotai',
+          'jotai/vanilla/utils': 'jotaiUtils',
         },
       },
     },
